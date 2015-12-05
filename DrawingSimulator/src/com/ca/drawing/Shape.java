81d1cdf8ef41ae448ac65b0d7f324ea47417471a
bunya <can.madran@davon.com.tr>
2015-12-05 22:29:49 +0200

    most pmd problems fixed

--- DrawingSimulator/src/com/ca/drawing/Shape.java
+++ DrawingSimulator/src/com/ca/drawing/Shape.java
@@ -6,30 +6,39 @@ import java.awt.Point;
 import java.util.HashSet;
 import java.util.Set;
 
+/** Abstract class that represents all renderable shapes */
+@SuppressWarnings("PMD.AbstractNaming")
 public abstract class Shape extends Drawable {
+	/** colour of the shape */
 	private Color color;
 
+	/** draws the shape on the canvas */
 	public abstract void draw(Canvas canvas);
 
-	public void fillColor(Color color) {
+	/** fills the shape with colour */
+	public void fillColor(final Color color) {
 		setColor(color);
 	}
 
-	private void setColor(Color color) {
+	private void setColor(final Color color) {
 		this.color = color;
 	}
 
+	/** @return colour of the shape */
 	public Color getColor() {
 		return color;
 	}
 
-	protected Double calculateDistance(Point p0, Point p1) {
-		return Point.distance(p0.getX(), p0.getY(), p1.getX(), p1.getY());
+	/** @return distance between two points */
+	protected Double calculateDistance(final Point point0, final Point point1) {
+		return Point.distance(point0.getX(), point0.getY(), point1.getX(),
+				point1.getY());
 	}
 
+	/** @return number of used colours */
 	@Override
 	public Set<Color> getUsedColors() {
-		Set<Color> colors = new HashSet<>();
+		final Set<Color> colors = new HashSet<>();
 		colors.add(color);
 		return colors;
 	}
